0df963cad1dccf4e7d6634ae1e391dec27ffe3e0
Sma Das <smaddas10@gmail.com>
2022-08-02 14:26:10 +0400

    Created heading and wave animation

--- sections/about-me.tsx
+++ sections/about-me.tsx
@@ -3,7 +3,13 @@ const AboutMe: React.FC = () => {
     <>
       <section id="about-me" className="h-screen flex flex-col">
         <div className="text-center py-5">
-          <h1 className="font-bold text-5xl drop-shadow-xl">About Me</h1>
+          <h1 className="font-bold text-3xl drop-shadow-xl xl:text-5xl">
+            About Me
+          </h1>
+          <h2 className="font-bold text-lg drop-shadow-xl xl:text-2xl -ml-[1em]">
+            Hi, I&#39;m Sma Das{" "}
+            <span className="animate-wave absolute origin-[80%_80%]">👋🏻</span>
+          </h2>
         </div>
         <div className="flex-grow p-5">content</div>
       </section>
