179eb147415e717654ea38fc3453c3dac3fa4ccf
Sma Das <smaddas10@gmail.com>
2022-08-03 00:57:27 +0400

    Completed About Me

--- sections/about-me.tsx
+++ sections/about-me.tsx
@@ -14,7 +14,39 @@ const AboutMe: React.FC = () => {
             <span className="animate-wave absolute origin-[80%_80%]">👋🏻</span>
           </h2>
         </div>
-        <div className="flex-grow p-5">content</div>
+        <div className="flex flex-col flex-grow p-5 justify-evenly">
+          <p className="flex-shrink-0 leading-loose">
+            I am currently a 3<sup>rd</sup> year student at Rochester Institute
+            of Technology pursing an undergraduate degree in Computing Security
+            with a minor in Economics.
+            <br />
+            <br className="md:hidden" />I have participated in multiple CTF
+            competitions as well as developing vunerable boxes using Docker
+            Containers.
+            <br />
+            <br className="md:hidden" />I also have a passion for Software
+            Development. I have extensive experience in <code>Python</code>,{" "}
+            <code>JavaScript</code> /<code>TypeScript</code>, <code>C</code>/
+            <code>C++</code>, <code>Java</code>, and I am currently learning{" "}
+            <code>Go</code> and <code>Rust</code>.
+          </p>
+          <p className="leading-loose">
+            My course work includes:
+            <br />
+            <br />
+            <ul className="list-item list-disc list-outside ml-10 marker:text-gray-400">
+              <li>Reverse Engineering and Malware Analysis</li>
+              <li>Computer System Forensics</li>
+              <li>Software Devlopment in Python and Java</li>
+              <li>Database and Data Modeling</li>
+              <li>System Administration 1 & 2</li>
+            </ul>
+          </p>
+          <p className="leading-loose">
+            My current academic standing rests at a cGPA of 3.84/4 with my
+            previous two semesters yielding a GPA of 3.91/4 and 4/4
+          </p>
+        </div>
       </section>
     </>
   );
