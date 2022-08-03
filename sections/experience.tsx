5fe27b395bb5bfe4c08fb9e86f2c293468dc8d68
Sma Das <smaddas10@gmail.com>
2022-08-04 03:00:22 +0400

    Update static components

--- sections/experience.tsx
+++ sections/experience.tsx
@@ -1,42 +1,41 @@
 import Grid from "../components/grid";
 import GridItem from "../components/grid-item";
 
+const experience = [
+  {
+    heading: "Cloud & Software Engineer Lead",
+    location: "Unitech",
+    duration: "May 2022 - Current",
+    gridImage: "/assets/company_logos/unitech.png",
+    description:
+      "Responsible for deploying a full backend using AWS to implement secure data and file storage using Lambda, S3 buckets, DynamoDB, and AWS Cognito. Made use of AWS Amplify to synchronize these technologies to service clients.",
+  },
+  {
+    heading: "Student Government Technology Coordinator",
+    location: "Rochester Institute of Technology Dubai",
+    duration: "Jan 2022 - Current",
+    gridImage: "/assets/company_logos/rit.png",
+    description:
+      "Responsible for the technology requirements of the Student Government; including the development and maintenance of the official website and app",
+  },
+  {
+    heading: "Database Developer",
+    location: "Rochester Institute of Technology Dubai",
+    duration: "Jan 2022 - May 2022",
+    gridImage: "/assets/company_logos/rit-logo.jpg",
+    description:
+      "Responsible for the technology requirements of the Student Government; including the development and maintenance of the official website and app",
+  },
+  {
+    heading: "Pentetration Tester",
+    location: "TryHackMe",
+    duration: "Jan 2021 - Dec 2021",
+    gridImage: "/assets/company_logos/tryhackme-logo.png",
+    description:
+      "Responsible for the technology requirements of the Student Government; including the development and maintenance of the official website and app",
+  },
+];
 const Experience: React.FC = () => {
-  const positions = [
-    {
-      heading: "Cloud & Software Engineer Lead",
-      location: "Unitech",
-      duration: "May 2022 - Current",
-      gridImage: "/assets/company_logos/unitech.png",
-      description:
-        "Responsible for deploying a full backend using AWS to implement secure data and file storage using Lambda, S3 buckets, DynamoDB, and AWS Cognito. Made use of AWS Amplify to synchronize these technologies to service clients.",
-    },
-    {
-      heading: "Student Government Technology Coordinator",
-      location: "Rochester Institute of Technology Dubai",
-      duration: "Jan 2022 - Current",
-      gridImage: "/assets/company_logos/rit.png",
-      description:
-        "Responsible for the technology requirements of the Student Government; including the development and maintenance of the official website and app",
-    },
-    {
-      heading: "Database Developer",
-      location: "Rochester Institute of Technology Dubai",
-      duration: "Jan 2022 - May 2022",
-      gridImage: "/assets/company_logos/rit-logo.jpg",
-      description:
-        "Responsible for the technology requirements of the Student Government; including the development and maintenance of the official website and app",
-    },
-    {
-      heading: "Pentetration Tester",
-      location: "TryHackMe",
-      duration: "Jan 2021 - Dec 2021",
-      gridImage: "/assets/company_logos/tryhackme-logo.png",
-      description:
-        "Responsible for the technology requirements of the Student Government; including the development and maintenance of the official website and app",
-    },
-  ];
-
   return (
     <>
       <section
@@ -48,9 +47,9 @@ const Experience: React.FC = () => {
             Experience
           </h1>
         </div>
-        <div className=" xl:mr-12 p-5 ">
+        <div className=" xl:mx-4 p-5 ">
           <Grid>
-            {positions.map((position, idx) => (
+            {experience.map((position, idx) => (
               <GridItem key={idx} {...position} />
             ))}
           </Grid>
