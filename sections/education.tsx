3d202328fdb9ad0a61aa9e2b9782d89dd485fbc7
Sma Das <smaddas10@gmail.com>
2022-08-04 02:10:39 +0400

    Created new sections

--- sections/education.tsx
+++ sections/education.tsx
@@ -1,13 +1,43 @@
+import Grid from "../components/grid";
+import GridItem from "../components/grid-item";
+
 const Education: React.FC = () => {
+  const positions = [
+    {
+      heading: "BSc Computing Security",
+      location: "Rochester Institute of Technology Dubai",
+      duration: "Jan 2020 - Dec 2023",
+      gridImage: "/assets/company_logos/rit-logo.jpg",
+      description:
+        "Responsible for the technology requirements of the Student Government; including the development and maintenance of the official website and app",
+    },
+    {
+      heading: "Pentetration Tester",
+      location: "TryHackMe",
+      duration: "Jan 2021 - Dec 2021",
+      gridImage: "/assets/company_logos/tryhackme-logo.png",
+      description:
+        "Responsible for the technology requirements of the Student Government; including the development and maintenance of the official website and app",
+    },
+  ];
   return (
     <>
-      <section id="education" className="h-screen flex flex-col">
+      <section
+        id="education"
+        className="min-h-screen flex flex-col after:border-b-2 after:w-4/5 after:mx-[10%] after:border-gray-500 bg-slate-50 pb-10 after:pt-10"
+      >
         <div className="text-center py-5">
           <h1 className="font-bold text-3xl drop-shadow-xl xl:text-5xl">
             Education
           </h1>
         </div>
-        <div className="flex-grow p-5">content</div>
+        <div className="flex-grow xl:mr-12 p-5">
+          <Grid>
+            {positions.map((position, idx) => (
+              <GridItem key={idx} {...position} />
+            ))}
+          </Grid>
+        </div>
       </section>
     </>
   );
--- sections/skills.tsx
+++ sections/skills.tsx
@@ -1,7 +1,10 @@
 const Skills: React.FC = () => {
   return (
     <>
-      <section id="skills" className="h-screen flex flex-col">
+      <section
+        id="skills"
+        className="min-h-screen flex flex-col after:border-b-2 after:w-4/5 after:mx-[10%] after:border-gray-500 bg-slate-50 pb-10 after:pt-10"
+      >
         <div className="text-center py-5">
           <h1 className="font-bold text-3xl drop-shadow-xl xl:text-5xl">
             Skills
