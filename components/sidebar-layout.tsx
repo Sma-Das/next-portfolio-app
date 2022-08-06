4ad29d1769ff34fddf5fbe71e65f487bc8c14e65
Sma Das <smaddas10@gmail.com>
2022-08-06 22:49:30 +0400

    Merged dev into main

--- components/sidebar-layout.tsx
+++ components/sidebar-layout.tsx
@@ -6,7 +6,7 @@ type SidebarLayoutProps = {
 
 const SidebarLayout: React.FC<SidebarLayoutProps> = ({ children }) => {
   return (
-    <div className="h-screen sticky top-0 hidden w-[13%]  md:flex items-center justify-evenly z-10 shadow-2xl flex-col bg-slate-50">
+    <div className="h-screen sticky top-0 hidden w-[14%] flex-shrink-0 md:flex items-center justify-evenly z-10 shadow-2xl flex-col bg-slate-50  overflow-x-hidden">
       {children}
     </div>
   );
