c0acc411cc4eceec567d563a5af663e3539ef11c
Sma Das <smaddas10@gmail.com>
2022-08-07 16:30:08 +0400

    Toggleble Skills

--- sections/skills.tsx
+++ sections/skills.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import React, { useState } from "react";
 
 type SkillItem = {
   imageSrc: string;
@@ -144,6 +145,8 @@ const SkillsList: { [heading: string]: Array<SkillItem> } = {
 const skillImageBase = "/assets/skills";
 
 const Skills: React.FC = () => {
+  const [active, setActive] = useState<string>("Programming Languages");
+
   return (
     <>
       <section
@@ -155,36 +158,49 @@ const Skills: React.FC = () => {
             Skills
           </h1>
         </div>
-        {Object.entries(SkillsList).map(([heading, skillItems], idx) => {
-          return (
-            <>
-              <h2 key={idx} className="text-center text-3xl my-2">
-                <span className="drop-shadow-xl">{heading}</span>
-              </h2>
-              <div className="grid grid-cols-3 md:grid-flow-col md:grid-cols-none gap-5 p-5  text-center justify-center mx-auto">
-                {skillItems.map(({ imageSrc, alt, width }, idx) => (
-                  <>
-                    <div
-                      className="w-full group relative pt-3 scale-90 xl:scale-95 hover:scale-105 xl:hover:scale-100"
-                      key={idx}
-                    >
-                      <div className="hidden group-hover:block absolute left-0 right-0 mx-auto z-10 bottom-0 translate-y-7 bg-white shadow-xl pb-1 rounded-md text-center pt-1 border">
-                        {alt}
-                      </div>
-                      <Image
-                        src={`${skillImageBase}/${imageSrc}`}
-                        width={width}
-                        height="100%"
-                        alt={alt}
-                        title={alt}
-                      />
+        {Object.entries(SkillsList).map(([heading, skillItems], idx) => (
+          <React.Fragment key={idx}>
+            <h2 key={idx} className="text-center text-xl md:text-3xl my-2">
+              <span onClick={() => setActive(heading)} className="">
+                {heading}
+                <span className="m-2 md:m-5">
+                  <Image
+                    src="/assets/icons/dark_arrow.png"
+                    height="15px"
+                    width="15px"
+                    alt="logo"
+                    className={`transition ${
+                      active === heading ? "rotate-180" : "rotate-0"
+                    }`}
+                  />
+                </span>
+              </span>
+            </h2>
+            <div className=" grid grid-cols-3 md:grid-flow-col md:grid-cols-none gap-5 p-5  text-center justify-center mx-auto">
+              {skillItems.map(({ imageSrc, alt, width }, id) => (
+                <React.Fragment key={id}>
+                  <div
+                    className={`w-full group relative pt-3 scale-90 xl:scale-95 hover:scale-105 xl:hover:scale-100 ${
+                      active === heading ? "" : "hidden"
+                    }`}
+                    key={imageSrc}
+                  >
+                    <div className="hidden group-hover:block absolute left-0 right-0 mx-auto z-10 bottom-0 translate-y-7 bg-white shadow-xl pb-1 rounded-md text-center pt-1 border">
+                      {alt}
                     </div>
-                  </>
-                ))}
-              </div>
-            </>
-          );
-        })}
+                    <Image
+                      src={`${skillImageBase}/${imageSrc}`}
+                      width={width}
+                      height="100%"
+                      alt={alt}
+                      title={alt}
+                    />
+                  </div>
+                </React.Fragment>
+              ))}
+            </div>
+          </React.Fragment>
+        ))}
       </section>
     </>
   );
